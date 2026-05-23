938f84c9d8318d4c1007ed28e55425941211ddf3
Talden-Key <tayho3893@gmail.com>
2026-05-23 00:26:14 -0700

    SQL: average delivery time

---
+++ sql/delivery_analysis.sql
@@ -0,0 +1,14 @@
+-- Average Delivery Time
+SELECT
+    ROUND(
+        AVG(
+            DATE_PART(
+                'day',
+                o.order_delivered_customer_date::timestamp
+                - o.order_purchase_timestamp::timestamp
+            )
+        ),
+        2
+    ) AS avg_delivery_days
+FROM orders o
+WHERE o.order_status = 'delivered';
