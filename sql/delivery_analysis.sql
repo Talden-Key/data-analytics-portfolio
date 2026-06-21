b3ffbd0fe165b58f6ef5bd9f4473b7ebf1b89dc6
Talden-Key <tayho3893@gmail.com>
2026-06-20 21:34:29 -0700

    SQL: Delivery Success Rate

--- sql/delivery_analysis.sql
+++ sql/delivery_analysis.sql
@@ -36,3 +36,14 @@ JOIN order_reviews r
     ON o.order_id = r.order_id
 
 GROUP BY 1;
+
+-- Delivery Success Rate
+SELECT
+    ROUND(
+        100.0 *
+        SUM(CASE WHEN order_status = 'delivered' THEN 1 ELSE 0 END)
+        / COUNT(*),
+        2
+    ) AS delivery_rate
+FROM orders;
+
