4d2bc1544dc6be52b0ea37d1447ff3582354f284
Talden-Key <tayho3893@gmail.com>
2026-07-10 23:19:45 -0700

    SQL:Compare late vs on-time deliveries

--- sql/delivery_analysis.sql
+++ sql/delivery_analysis.sql
@@ -47,3 +47,21 @@ SELECT
     ) AS delivery_rate
 FROM orders;
 
+--Review Score vs delivery outcome
+--Compare Late vs On-time Deliveries
+SELECT
+    CASE
+        WHEN order_delivered_customer_date > order_estimated_delivery_date
+            THEN 'Late'
+        ELSE 'On Time'
+        END AS delivery_status,
+        COUNT(*) AS total_orders,
+        ROUND(AVG(review_score),2) AS avg_review_score
+
+FROM orders o
+
+JOIN order_reviews r 
+ON o.order_id = r.order_id 
+WHERE order_status = 'delivered'
+GROUP BY 1;
+
