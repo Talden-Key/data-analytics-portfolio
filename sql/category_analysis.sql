3a656db0891c1959b77a16d4cfa8dd1edabc56e3
Talden-Key <tayho3893@gmail.com>
2026-07-15 20:28:57 -0700

    :SQL:Shipping cost by category

--- sql/category_analysis.sql
+++ sql/category_analysis.sql
@@ -17,3 +17,13 @@ JOIN products p
     ON oi.product_id = p.product_id
 GROUP BY p.product_category_name
 ORDER BY avg_price DESC;
+
+--Shipping Cost by Category
+SELECT 
+    p.product_category_name,
+    ROUND(AVG(oi.freight_value),2) AS avg_shipping_cost
+FROM order_items oi
+JOIN products p
+    ON oi.product_id = p.product_id
+GROUP BY p.prodcut_category_name
+ORDER BY avg_shipping_cost DESC;
